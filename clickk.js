60bb6582cd27702563d70ebab7ee9022ac70923a
Sabuciss <162728378+Sabuciss@users.noreply.github.com>
2024-11-26 15:08:37 +0200

    Create clickk.js

---
+++ clickk.js
@@ -0,0 +1,21 @@
+function clickCounter() {
+    if (typeof(Storage) !== "undefined") {
+      if (localStorage.clickcount) {
+        localStorage.clickcount = Number(localStorage.clickcount)+1;
+      } else {
+        localStorage.clickcount = 1;
+      }
+      document.getElementById("result").innerHTML = "Tu esi nospiedis:  " + localStorage.clickcount + " time(s).";
+    } else {
+      document.getElementById("result").innerHTML = "Sowwyy";
+    }
+  }
+
+
+let mainig = 0 ;
+ 
+function clickk(){
+      console.log("strada");
+      mainig=mainig + 1 ; 
+document.getElementById("poga").innerHTML= mainig;
+  }
